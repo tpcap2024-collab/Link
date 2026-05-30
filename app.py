1cb0fa89f0d516701ec77169b95a4dfecabd13ef
TFR AI <tpcap2024@gmail.com>
2026-05-30 09:17:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+import requests
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 
@@ -9,7 +12,18 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
+
+    img_url = data["photo"]
+
+    # โหลดรูปจาก URL
+    img_bytes = requests.get(img_url).content
+    img_array = np.frombuffer(img_bytes, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+    # TODO: ใส่ AI วิเคราะห์จริงตรงนี้ก่อน
+    fill_rate = 78
+
     return {
-        "status": "received",
-        "data": data
+        "status": "ok",
+        "fill_rate": fill_rate
     }
